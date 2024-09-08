efc925dc31160a31f839ddfabb5fc5680c6b5127
Muzhgan Shams <muzhgan_shams@yahoo.com>
2024-09-08 22:50:57 +0500

    Add some JS behavior for form

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
+function generateFact(event) {
+  event.preventDefault();
 
+  new Typewriter('#fact', {
+    strings:
+      ' Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur.',
+    autoStart: true,
+    delay: 1,
+    cursor: '',
+  });
+}
+
+let factFormElement = document.querySelector('#form-element');
+factFormElement.addEventListener('submit', generateFact);
